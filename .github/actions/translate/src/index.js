14117e02818036b4a6e40e8b55a1274aa445db13
GitHub <41898282+github-actions[bot]@users.noreply.github.com>
2020-11-17 00:52:24 +0800

    feat: add local action

---
+++ .github/actions/translate/src/index.js
@@ -0,0 +1,82 @@
+const core = require("@actions/core");
+const tencentcloud = require("tencentcloud-sdk-nodejs");
+const path = require("path");
+const githubWorkspace =
+  process.env.GITHUB_WORKSPACE || path.resolve(__dirname, "../../../../");
+async function main() {
+  const TmtClient = tencentcloud.tmt.v20180321.Client;
+
+  const clientConfig = {
+    credential: {
+      secretId: core.getInput("secret_id"),
+      secretKey: core.getInput("secret_key"),
+    },
+    region: "na-siliconvalley",
+    profile: {
+      httpProfile: {
+        endpoint: "tmt.tencentcloudapi.com",
+      },
+    },
+  };
+
+  const client = new TmtClient(clientConfig);
+  const locales = ["zh"];
+  const allYears = getAllYears();
+  for (let i = 0; i < allYears.length; i++) {
+    const year = allYears[i];
+    const redditEnTitleFilePath = `i18n/i18next/en/reddit-title-${year}.json`;
+    const enTitle = require(`${githubWorkspace}/${redditEnTitleFilePath}`);
+    for (let j = 0; j < locales.length; j++) {
+      const locale = locales[j];
+      const redditLocaleTitleFilePath = `i18n/i18next/${locale}/reddit-title-${year}.json`;
+      const localeTitle = require(`${githubWorkspace}/${redditLocaleTitleFilePath}`);
+      // check
+      const enKeys = Object.keys(enTitle);
+      let isChanged = false;
+      for (let k = 0; k < enKeys.length; k++) {
+        const key = enKeys[k];
+        if (!localeTitle[key]) {
+          isChanged = true;
+          const params = {
+            SourceText: key,
+            Source: "en",
+            Target: locale,
+            ProjectId: 0,
+          };
+          console.log("params", params);
+
+          // const data = await client.TextTranslate(params);
+          // // request
+          // localeTitle[key] = data.TargetText;
+        }
+      }
+      // if changed
+      if (isChanged) {
+        // write
+        console.log("write");
+      }
+    }
+  }
+}
+
+const getAllYears = () => {
+  const currentYears = new Date().getUTCFullYear();
+  const allYears = [];
+  for (let i = 2020; i <= currentYears + 1; i++) {
+    allYears.push(i);
+  }
+  return allYears;
+};
+main().catch((e) => {
+  core.setFailed(e);
+});
+
+module.exports = (
+  apiKey,
+  text,
+  lang,
+  addParam,
+  addParam2,
+  addParam3,
+  addParam4
+) => {};
