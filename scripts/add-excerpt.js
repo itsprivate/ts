2f47b60b6838c4648df83c9d2e0737de0a7018af
GitHub <41898282+github-actions[bot]@users.noreply.github.com>
2020-12-07 21:53:11 +0800

    feat: add excerpt

---
+++ scripts/add-excerpt.js
@@ -0,0 +1,34 @@
+const format = require("../.github/actions/format/src/format/reddit");
+const micromatch = require("micromatch");
+const { resolve, relative } = require("path");
+const { readdir, readFile, writeFile } = require("fs").promises;
+async function main() {
+  const files = await getFiles(resolve(__dirname, "../data/reddit-top"));
+  const jsonFiles = micromatch(files, "**/*.json");
+  for (let i = 0; i < jsonFiles.length; i++) {
+    const jsonPath = resolve(__dirname, "../", jsonFiles[i]);
+    const jsonContent = await readFile(jsonPath, "utf8");
+    let json = JSON.parse(jsonContent);
+    if (!json.__excerpt && json.selftext_html) {
+      const newJson = format([json]);
+      console.log("write", jsonPath);
+      await writeFile(jsonPath, JSON.stringify(newJson[0], null, 2));
+    }
+  }
+}
+
+main().catch((e) => {
+  console.error("e", e);
+});
+
+async function getFiles(dir) {
+  const cwd = process.cwd();
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : relative(cwd, res);
+    })
+  );
+  return Array.prototype.concat(...files);
+}
